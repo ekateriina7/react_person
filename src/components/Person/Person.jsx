194c014b633040e168a3b1e17b25cdb4359bcd3e
katya havriuk <0303gavkat@gmail.com>
2024-04-11 09:21:34 -0600

    fix nested ternary

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,21 @@
 export const Person = ({ person }) => {
+  const getPartnerTitle = (sex, isMarried) => {
+    if (!isMarried) {
+      return '';
+    }
+
+    return sex === 'm' ? 'wife' : 'husband';
+  };
+
+  const partnerTitle = getPartnerTitle(person.sex, person.isMarried);
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">I am {person.age}</p>}
       <p className="Person__partner">
         {person.isMarried
-          ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+          ? `${person.partnerName} is my ${partnerTitle}`
           : 'I am not married'}
       </p>
     </section>
